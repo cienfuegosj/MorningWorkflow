9acd5fbca745a83fc08dc9376bc98ec096974490
cienfuegosj <cienfuegosj@berea.edu>
2018-06-22 17:53:04 -0500

    Initialized Repository

---
+++ Workflow/ApplicationStarter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Script;
+using System.Threading.Tasks;
+
+namespace Workflow {
+    class ApplicationStarter {
+        /*
+         * Application Starter reads the "StartupApps.json" file, parses the applications in
+         * the program files available, starts the executables, and releases them at the user
+         * level. */
+        ApplicationStarter(String StartupAppsJSONFilename /* Debugger Object */)
+        {
+            if (File.Exists(StartupAppsJSONFilename))
+            {
+                String s_Content = File.ReadAllText(StartupAppsJSONFilename);
+                var JSON_Serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+                var StartupApps = JSON_Serializer.Deserialize<Dictionary<string, Dictionary<string, string>>[]>(s_Content);
+            }
+            else
+            {
+                // Debugging Info must be outputted
+            }        
+        }
+    }
+}
---
+++ Workflow/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Start up list of applications given in the specified file: "StartupApps.json"
+             */
+        }
+    }
+}
