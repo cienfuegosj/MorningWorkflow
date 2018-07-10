7f6a887902def5ed86deee2d34053efcbef8d958
Javier Cienfuegos <cienfuegosj@berea.edu>
2018-07-09 22:50:13 -0500

    Adding changes to Debug Handler.

--- Workflow/DebugHandler.cs
+++ Workflow/DebugHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,34 @@ namespace Workflow
 {
     class DebugHandler
     {
-        DebugHandler(String? DebugFileName)
+        DebugHandler(String DebugFileName = null)
         {
-
+            // Check if the Workflow Metadata Debug Folder and Path is valid
         }
+        private void ValidateMetaDebugFolderandPath()
+        {
+            /*
+             * Check if environment variable is set and if not, set the environment variable
+             * WORKFLOW_META to user's Program Files/MorningWorkflow/Meta folder.
+            */
+            if(Environment.GetEnvironmentVariable("WORKFLOW_META") != null)
+            {
+                // Get file and log Debug Handler connection
+                string s_WORKFLOW_META_path = Environment.GetEnvironmentVariable("WORKFLOW_META");
+                s_WORKFLOW_META_path.Trim();
+                try
+                {
+                    FileStream f_WORKFLOW_META = File.OpenWrite(s_WORKFLOW_META_path);
+                    f_WORKFLOW_META.Write();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+        
+
     }
 }
---
+++ Workflow/StringConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workflow
+{
+    static class StringConstants
+    {
+        public const string STRING_DEBUG_HANDLER_CONNECTED = "DEBUG HANDLER CONNECTED";
+        public const string STRING_DEBUG_HANDLER_DISCONNECTED = "DEBUG HANDLER DISCONNECTED";
+    }
+}
